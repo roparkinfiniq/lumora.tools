020581d3dd4766844057872ab50c8e091dc32b61
Lumora <lumora@lumora.tools>
2026-06-21 10:25:38 +0900

    feat: delete example post 'The perfect cup of pour-over'

--- src/data/posts/index.ts
+++ src/data/posts/index.ts
@@ -2,7 +2,6 @@ import { BlogPost } from "../../types";
 import { stillness } from "./stillness";
 import { paperNotebook } from "./paper-notebook";
 import { weekendWithoutPlans } from "./weekend-without-plans";
-import { perfectPourOver } from "./perfect-pour-over";
 import { needRest } from "./need-rest";
 
 export const blogPosts: BlogPost[] = [
@@ -10,5 +9,4 @@ export const blogPosts: BlogPost[] = [
   stillness,
   paperNotebook,
   weekendWithoutPlans,
-  perfectPourOver,
 ];
